5114a34fb833d36689fbf63836d06b0d16bdd8a8
DongHwan <dhpak97@naver.com>
2020-05-17 15:26:25 +0900

    다시푸쉬

--- CafeReservationSystem/src/user/User.java
+++ CafeReservationSystem/src/user/User.java
@@ -1,5 +1,40 @@
 package user;
 
 public class User {
-
+	private String userID;
+	private String userPW;
+	private String Name;
+	private String Birth;
+	private boolean Blacklist;
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPW() {
+		return userPW;
+	}
+	public void setUserPW(String userPW) {
+		this.userPW = userPW;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getBirth() {
+		return Birth;
+	}
+	public void setBirth(String birth) {
+		Birth = birth;
+	}
+	public boolean isBlacklist() {
+		return Blacklist;
+	}
+	public void setBlacklist(boolean blacklist) {
+		Blacklist = blacklist;
+	}
 }
--- CafeReservationSystem/src/user/UserDAO.java
+++ CafeReservationSystem/src/user/UserDAO.java
@@ -1,5 +1,45 @@
 package user;
 
-public class UserDAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class UserDAO {
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3365/데이터베이스이름";
+			String dbID="root";
+			String dbPW= "계정패스워드";
+			Class.forName("com.mysql.jdbc.Driver");
+			conn=DriverManager.getConnection(dbURL,dbID,dbPW);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int login(String userID, String userPW) {
+		String SQL="SELCECT userPW FROM USER WHERE userID=?";
+		try {
+			pstmt=conn.prepareStatement(SQL);
+			pstmt.setString(1,userID);
+			rs=pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPW))
+					return 1;//로그인성공
+				else
+					return 0;//비밀번호 불일치
+	
+			}
+			return -1;//아이디 db에 미존재
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -2;
+	}
 }
